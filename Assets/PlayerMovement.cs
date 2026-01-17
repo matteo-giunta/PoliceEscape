ce36ef001122348c7e3af793402db3df7a6a93e2
Matteo Giunta <mat.giunta@stud.uniroma3.it>
2026-01-17 02:58:24 +0100

    Fixed input system and player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY);
+        rb.linearVelocity = movement * speed;
+    }
+}
